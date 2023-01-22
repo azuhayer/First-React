67cdb934e10c1a8251cf01190c7b2c099d206d9a
Zuhayer Alvi <azuhayer@gmail.com>
2023-01-22 13:27:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-ReactDOM.render(<ul>
-                    <li>Hello</li>
-                    <li>Goodbye</li>
-                </ul>, document.getElementById("root"))
+function NavBar() {
+    return (
+        <h3>hahahaha</h3>
+    )
+}
+
+function MainContent() {
+    return (
+        <h1>Hello my name is Zuhayer!</h1>
+    )
+}
+
+ReactDOM.render(<div>
+                    <NavBar />
+                    <MainContent />
+                </div>
+                , document.getElementById("root"))
